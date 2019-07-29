b65ddcc9bcf436e4383ce8c6516eb0a753c1c034
Wassim Hajji <49130482+wasshj@users.noreply.github.com>
2019-07-29 13:47:07 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,21 +1,21 @@
 exports.config = {
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['--no-sandbox']
-        }
-    },
-    directConnect: true,
-    baseUrl: 'https://testing-angular-applications.github.io',
-
-    framework: 'jasmine',
-    specs: [
-        './e2e/*.e2e-spec.ts'
-    ],
-    onPrepare: () => {
-        require('ts-node').register({
-            project: 'e2e'
-        });
-    },
-    useAllAngular2AppRoots: true
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--no-sandbox']
+    }
+  },
+  directConnect: true,
+  baseUrl: 'https://testing-angular-applications.github.io',
+  
+  framework: 'jasmine',
+  specs: [
+    './e2e/test-edit.e2e-spec.ts'
+  ],
+  onPrepare: () => {
+    require('ts-node').register({
+      project: 'e2e'
+    });
+  },
+  useAllAngular2AppRoots: true
 };
