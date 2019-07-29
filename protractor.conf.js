d945002a82b799c364ad1e67b4069c68e3dd2415
Wassim Hajji <49130482+wasshj@users.noreply.github.com>
2019-07-29 20:26:05 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -43,22 +43,15 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare: ()=> {
-    if (process.env.IS_JENKINS) {
-      let jasmineReporters = require('jasmine-reporters');
-      let junitReporter = new jasmineReporters.JUnitXmlReporter({
-        savePath: 'output/',
-        consolidateAll: false
-      });
-      jasmine.getEnv().addReporter(junitReporter);
-    } else {
-      let specReporter = new SpecReporter({
-        spec: { displayStacktrace: true }
-      });
-      jasmine.getEnv().addReporter(specReporter);
-    }
-    require('ts-node').register({
-      project: 'e2e/tsconfig.json'
-    });
-  }
+  onPrepare: function(){
+        require('jasmine-reporters');
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function(caps){
+            var browserName = caps.caps_.browserName.toUpperCase();
+            var browserVersion = caps.caps_.version;
+            var prePendStr = browserName + "-" + browserVersion + "-";
+            jasmine.getEnv().addReporter(new
+jasmine.JUnitXmlReporter("protractor_output", true, true,prePendStr));
+        });
+ }
 };
