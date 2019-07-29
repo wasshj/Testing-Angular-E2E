65c77f60516b430ef6410d6f5465af76b7c27733
Wassim Hajji <49130482+wasshj@users.noreply.github.com>
2019-07-29 11:42:20 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,45 +1,19 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
-const { SpecReporter } = require('jasmine-spec-reporter');
-
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    'e2e/**/*.e2e-spec.ts'
-  ],
+  directConnect: false,    
   capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: (process.env.IS_CIRCLE ? ['--headless'] : [])
-    }
+    browserName: 'chrome'  
   },
-  directConnect: !process.env.IS_JENKINS,
   baseUrl: 'https://testing-angular-applications.github.io',
-
-  // Jasmine
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare: ()=> {
-    if (process.env.IS_JENKINS) {
+  specs: ['e2e/*.spec.ts'],
+  onPrepare: () => {  
       let jasmineReporters = require('jasmine-reporters');
       let junitReporter = new jasmineReporters.JUnitXmlReporter({
         savePath: 'output/',
         consolidateAll: false
       });
       jasmine.getEnv().addReporter(junitReporter);
-    } else {
-      let specReporter = new SpecReporter({
-        spec: { displayStacktrace: true }
-      });
-      jasmine.getEnv().addReporter(specReporter);
-    }
     require('ts-node').register({
-      project: 'e2e/tsconfig.json'
+      project: 'e2e'
     });
-  }
+  },
 };
