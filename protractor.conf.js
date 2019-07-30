0321f6c077e0e94c241b11a48e3b95acff8cf108
Wassim Hajji <49130482+wasshj@users.noreply.github.com>
2019-07-30 13:26:57 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,14 +9,12 @@ exports.config = {
   baseUrl: 'https://testing-angular-applications.github.io',
   specs: ['e2e/**/*.e2e-spec.ts'],
   onPrepare: () => {
-    if (process.env.IS_JENKINS) {
       let jasmineReporters = require('jasmine-reporters');
       let junitReporter = new jasmineReporters.JUnitXmlReporter({
         savePath: 'output/',
         consolidateAll: false
       });
       jasmine.getEnv().addReporter(junitReporter);
-    }
     require('ts-node').register({
       project: 'e2e'
     });
